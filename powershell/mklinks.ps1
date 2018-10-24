6d256169120abe69674204eb922b26135689a1c8
dabape <dabape@gmail.com>
2018-10-24 10:52:04 +0200

    Update paths

--- powershell/mklinks.ps1
+++ powershell/mklinks.ps1
@@ -1,2 +1,2 @@
 New-Item -Path $home\.vim -ItemType SymbolicLink -Value  $home\dotfiles\vim
-New-Item -Path $home\.vimrc -ItemType SymbolicLink -Value  $home\dotfiles\vimrc
+New-Item -Path $home\.vimrc -ItemType SymbolicLink -Value  $home\dotfiles\vim\vimrc
